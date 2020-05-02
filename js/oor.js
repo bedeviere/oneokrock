fbffb4235baf95137656da521f37ef9cd59e8530
Bimata Prathama <bimata.prathama@gmail.com>
2020-05-02 01:02:42 -0700

    added merch page and tour dates page

--- js/oor.js
+++ js/oor.js
@@ -1,3 +1,39 @@
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("merch-item");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
 $(document).ready(function() {
 
   // Check for click events on the navbar burger icon
@@ -8,4 +44,18 @@ $(document).ready(function() {
       $(".navbar-menu").toggleClass("is-active");
 
   });
+
+  filterSelection("all")
+
+  // Add active class to the current control button (highlight it)
+  var merchButtons = document.getElementById("merchButtons");
+  var merchButton = merchButtons.getElementsByClassName("merch-button");
+  for (var i = 0; i < merchButton.length; i++) {
+    merchButton[i].addEventListener("click", function() {
+      var current = merchButtons.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+
 });
